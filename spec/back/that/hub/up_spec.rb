ea205071b22d12d8e1996929287f48f6ae16be85
Nat Welch <nat@natwelch.com>
2013-12-05 08:41:43 -0800

    +bin +tests

---
+++ spec/back/that/hub/up_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Back::That::Hub::Up do
+  it 'should have a version number' do
+    Back::That::Hub::Up::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end
